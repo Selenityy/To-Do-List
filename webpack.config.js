3bf865e70259e2671ba2c5e670e54ebd1a36b6bf
Selena <selena.k.rodriguez@gmail.com>
2023-03-21 18:39:22 -0500

    Initial set up and save

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: " ",
+    assetModuleFilename: "images/[name][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpeg|jpg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
