68548ef0f5f18257d240107dd7c62d958ec9ea98
Selena <selena.k.rodriguez@gmail.com>
2023-03-22 17:52:46 -0500

    Created the menu layout and added the images

--- src/pages/1. Projects Only/allProjects.js
+++ src/pages/1. Projects Only/allProjects.js
@@ -2,9 +2,54 @@ import {
   createNewDiv,
   setClassAttr,
   addText,
+  createImg,
+  addBtn,
+  addUl,
+  addLi,
   addEventListClick,
+  addHref,
+  addPara,
+  createPara,
 } from "../../functions/DOMlogic";
+import allTaskImg from "../../assets/all-tasks.png";
+import todayTasksImg from "../../assets/today-tasks.png";
+import weekTasksImg from "../../assets/this-week-task.png";
 
 export default function allProjects() {
+  // Projects Page Div within Body
+  createNewDiv("allProjectsPage", "body");
+  setClassAttr("allProjectsPage", "allProjectsPageContent");
 
+  // Menu Content
+  createNewDiv("menu", "allProjectsPage");
+  setClassAttr("menu", "allProjectsPageContent");
+
+  // Menu - Current Tasks
+  createNewDiv("currentTasks", "menu");
+
+  createNewDiv("allTasks", "currentTasks");
+  createImg("allTaskImg", "allTasks", allTaskImg);
+  createPara("All Tasks", "allTasks");
+
+  createNewDiv("todayTasks", "currentTasks");
+  createImg("todayTaskImg", "todayTasks", todayTasksImg);
+  createPara("Today's Tasks", "todayTasks");
+
+  createNewDiv("thisWeekTasks", "currentTasks");
+  createImg("weekTaskImg", "thisWeekTasks", weekTasksImg);
+  createPara("This Week's Tasks", "thisWeekTasks");
+
+  // Menu - Projects
+  //   createNewDiv("allProjects", "menu");
+  addUl("allProjects", "menu");
+  addText("allProjects", "All Projects");
+
+  // Add Project Button
+  addBtn("addProject", "allProjects");
+  addText("addProject", "+");
+  // add code when button is clicked to add li's to the ul of each new project
+
+  // Projects Body Content
+  createNewDiv("mainContent", "allProjectsPage");
+  setClassAttr("mainContent", "allProjectsPageContent");
 }
