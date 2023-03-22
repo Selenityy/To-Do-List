2091bb39d04eb2b40932377f8b89ab1bfc83257a
Selena <selena.k.rodriguez@gmail.com>
2023-03-21 20:43:25 -0500

    Created the header, body and footer divs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('hello');
+import allProjects from "./pages/1. Projects Only/allProjects";
+import {
+  createNewDiv,
+  setClassAttr,
+  addText,
+  addEventListClick,
+} from "./functions/DOMlogic";
+
+// Create Header
+createNewDiv("header", "content");
+setClassAttr("header", "headerBody");
+
+// Create Body
+createNewDiv("body", "content");
+setClassAttr("body", "classBody");
+
+// Create Footer
+createNewDiv("footer", "content");
+setClassAttr("footer", "footerBody");
