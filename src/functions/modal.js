2e012ec9cf85be8a7e243fa40bcf9d0f6dab835c
Selena <selena.k.rodriguez@gmail.com>
2023-03-31 15:48:20 -0500

    added a close button function that deletes the modal form. Also added in a object constructor for the project and the form values to the submit button

--- src/functions/modal.js
+++ src/functions/modal.js
@@ -1,9 +1,12 @@
+import { createNewDiv } from "./DOMlogic";
+import { removeModalForm } from "./applicationLogic";
+
 const createModal = () => {
   let template = `
     <h2>New Project</h2>
     <div id="myModal" class="modal">
     <div class="modal_content">
-      <span class="close">&times;</span>
+      <span id="close" class="close">&times;</span>
       <form name="myForm" id="myForm" action="" method="GET">
         <ul class="pop_up">
           <li class="title_form">
@@ -51,15 +54,48 @@ const createModal = () => {
     </div>
   </div>
   `;
-  let mainContent = document.getElementById("mainContent");
-  mainContent.innerHTML = template;
+  createNewDiv("modalDiv", "mainContent");
+  let modalDiv = document.getElementById("modalDiv");
+  modalDiv.innerHTML = template;
 
+  let closeBtn = document.getElementById("close");
+  closeBtn.onclick = function (event) {
+    removeModalForm();
+  };
+
+  let myProjects = [];
+
+  // Project Object Constructor
+  function Project(name, description, dueDate, priority, notes) {
+    this.name = name;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+  }
+
+  // Submit Btn triggers the following: grabs values, adds to Project Obj
   let submitBtn = document.getElementById("submitBtn");
   submitBtn.onclick = function (event) {
-    console.log("HELLO");
-    // form.reset();
+    const formSubmissionNewProjectName = document.getElementById("name").value;
+    const formSubmissionNewProjectDescription =
+      document.getElementById("description").value;
+    const formSubmissionNewProjectDueDate =
+      document.getElementById("dueDate").value;
+    const formSubmissionNewProjectPriority =
+      document.getElementById("priority").value;
+    const formSubmissionNewProjectNotes =
+      document.getElementById("notes").value;
+    const addProjectObj = new Project(
+      formSubmissionNewProjectName,
+      formSubmissionNewProjectDescription,
+      formSubmissionNewProjectDueDate,
+      formSubmissionNewProjectPriority,
+      formSubmissionNewProjectNotes
+    );
+    myProjects.push(addProjectObj);
+
     event.preventDefault();
-    console.log("BYE");
   };
 };
 
