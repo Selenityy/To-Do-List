75040ee255bc97852022f4f088358d8fecf9ee26
Selena <selena.k.rodriguez@gmail.com>
2023-04-12 17:06:24 -0500

    cleaned up commented out code

--- src/functions/modal.js
+++ src/functions/modal.js
@@ -3,6 +3,7 @@ import {
   dateFormat,
   addProjectToMenu,
   addProjectToPage,
+  listenForClickOnProject,
 } from "./applicationLogic";
 
 export let myProjects = JSON.parse(localStorage.getItem("myProjects")) || [];
@@ -133,127 +134,5 @@ const createModal = () => {
     removeModalForm();
   };
 };
-
-// const addProjectToPage = (projectList) => {
-//   document.getElementById("newProjectDiv").innerHTML = "";
-//   for (let i = 0; i < projectList.length; i++) {
-//     // Create new divs
-//     let formattedName = projectList[i].name.replace(/ +/g, "-");
-//     createNewDiv(formattedName, "newProjectDiv");
-//     setClassAttr(formattedName, "userProjectList");
-
-//     addBtn(`newProjectCompletedBtn${i}`, formattedName);
-//     let projectCompletionCheck =
-//       projectList[i].completed === true ? "checked" : "unchecked";
-//     document
-//       .getElementById(`newProjectCompletedBtn${i}`)
-//       .classList.add(projectCompletionCheck);
-
-//     addBtn(`newProjectRemoveBtn${i}`, formattedName);
-//     addText(`newProjectRemoveBtn${i}`, "X");
-//     setClassAttr(`newProjectRemoveBtn${i}`, "removeBtn");
-
-//     createNewDiv(`newProjectName${i}`, formattedName);
-//     setClassAttr(`newProjectName${i}`, "userProjectNames");
-
-//     createNewDiv(`newProjectDescription${i}`, formattedName);
-//     setClassAttr(`newProjectDescription${i}`, "userProjectDescription");
-
-//     createNewDiv(`newProjectDueDate${i}`, formattedName);
-//     setClassAttr(`newProjectDueDate${i}`, "userProjectDueDate");
-
-//     createNewDiv(`newProjectPriority${i}`, formattedName);
-//     setClassAttr(`newProjectPriority${i}`, "userProjectPriority");
-
-//     createNewDiv(`newProjectNotes${i}`, formattedName);
-//     setClassAttr(`newProjectNotes${i}`, "userProjectNotes");
-
-//     // Change innerHTML of the divs to the projects array
-//     document.getElementsByClassName("userProjectNames")[i].innerHTML =
-//       projectList[i].name;
-//     if (projectList[i].description === "") {
-//       document.getElementsByClassName("userProjectDescription")[i].innerHTML =
-//         "";
-//     } else {
-//       document.getElementsByClassName("userProjectDescription")[i].innerHTML =
-//         "Description: " + projectList[i].description;
-//     }
-//     if (projectList[i].dueDate === "") {
-//       document.getElementsByClassName("userProjectDueDate")[i].innerHTML = "";
-//     } else {
-//       document.getElementsByClassName("userProjectDueDate")[i].innerHTML =
-//         "Due Date: " + projectList[i].dueDate;
-//     }
-//     if (projectList[i].priority === "-") {
-//       document.getElementsByClassName("userProjectPriority")[i].innerHTML = "";
-//     } else {
-//       document.getElementsByClassName("userProjectPriority")[i].innerHTML =
-//         "Priority Level: " + projectList[i].priority;
-//     }
-//     if (projectList[i].notes === "") {
-//       document.getElementsByClassName("userProjectNotes")[i].innerHTML = "";
-//     } else {
-//       document.getElementsByClassName("userProjectNotes")[i].innerHTML =
-//         "Notes: " + projectList[i].notes;
-//     }
-
-//     // Mark Project as done
-//     let completedBtn = document.getElementById(`newProjectCompletedBtn${i}`);
-//     completedBtn.onclick = function (completedEvent) {
-//       let completedProjectName = completedEvent.target.parentElement.id;
-//       let result = myProjects.find(
-//         (project) => project.name === completedProjectName
-//       );
-//       if (completedEvent.target.classList.contains("unchecked")) {
-//         completedEvent.target.classList.remove("unchecked");
-//         completedEvent.target.classList.add("checked");
-//         result.completed = true;
-//         let title = document.getElementById(`onlyProjectName${i}`);
-//         title.style.setProperty("text-decoration", "line-through");
-//       } else {
-//         completedEvent.target.classList.remove("checked");
-//         completedEvent.target.classList.add("unchecked");
-//         result.completed = false;
-//         let title = document.getElementById(`onlyProjectName${i}`);
-//         title.style.setProperty("text-decoration", "none");
-//       }
-//       localStorage.setItem(`myProjects`, JSON.stringify(myProjects));
-//     };
-
-//     // Remove Project Btn
-//     let removeBtn = document.getElementById(`newProjectRemoveBtn${i}`);
-//     removeBtn.onclick = function (removeEvent) {
-//       const deleteProject =
-//         removeEvent.target.parentElement.children[2].innerHTML;
-//       myProjects = projectList.filter(
-//         (project) => project.name !== deleteProject
-//       );
-//       localStorage.setItem(`myProjects`, JSON.stringify(myProjects));
-//       addProjectToPage(myProjects);
-//       addProjectToMenu(myProjects);
-//     };
-//   }
-// };
-
-// const addProjectToMenu = (projectList) => {
-//   document.getElementById("allProjectsList").innerHTML = "";
-//   for (let i = 0; i < projectList.length; i++) {
-//     addLi(`liProjectName${i}`, "allProjectsList");
-//     setClassAttr(`liProjectName${i}`, "userProjectLi");
-
-//     createNewDiv(`onlyProjectName${i}`, `liProjectName${i}`);
-//     setClassAttr(`onlyProjectName${i}`, "menuProjectName");
-
-//     document.getElementsByClassName("menuProjectName")[i].innerHTML =
-//       projectList[i].name;
-//     if (projectList[i].completed === true) {
-//       let title = document.getElementById(`onlyProjectName${i}`);
-//       title.style.setProperty("text-decoration", "line-through");
-//     } else {
-//       let title = document.getElementById(`onlyProjectName${i}`);
-//       title.style.setProperty("text-decoration", "none");
-//     }
-//   }
-// };
 
 export { createModal };
